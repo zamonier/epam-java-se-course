c53edaef7b0f05802ae6ffe97f92b51633bd2df6
Eugene Mercuriev <eugene.mercuriev@gmail.com>
2017-03-18 11:08:58 +0300

    queue

---
+++ src/main/java/unit6/QueueExample.java
@@ -0,0 +1,42 @@
+package unit6;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.function.Function;
+
+/**
+ * Created by Eugene on 18.03.17.
+ */
+public class QueueExample {
+
+    public static void main(String[] args) {
+
+        queueApply(getQueue(LinkedList.class), Queue::poll);
+
+        queueApply(getQueue(LinkedList.class), Queue::remove);
+
+    }
+
+    private static void queueApply(Queue<String> queue, Function<Queue<String>, String> function) {
+        while (queue.size() > 0) {
+            System.out.print(function.apply(queue));
+            System.out.println(queue);
+        }
+    }
+
+
+    public static Queue<String> getQueue(Class<? extends Queue> cl) {
+        try {
+            Queue<String> q = (Queue<String>) cl.newInstance();
+            q.offer("Oklahoma");
+            q.offer("Indiana");
+            q.offer("Georgia");
+            q.offer("Texas");
+            return q;
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        throw new IllegalArgumentException();
+    }
+}
